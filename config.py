07545326aab93579c4feb04a169c2cc0669664a3
iSimpp <103945070+iSimpp@users.noreply.github.com>
2024-08-01 20:07:53 +0200

    Update config.py

--- config.py
+++ config.py
@@ -3,8 +3,14 @@ class Settings:
     images = ["read.png", "unread.png", "unread2.png"] # All images to check for.
     texts = [
     "Why did the chicken cross the road? To get to the other side :rofl::rofl::rofl::rofl::rofl::rofl::rofl::rofl::joy_cat::joy_cat::joy_cat::face_with_hand_over_mouth:",
-    "What did the gods  say to the pharaoh? Hark, mere mortal, for you have betrayed your land. Because of this act, you shall be banished to a land of no return. करहलततहपरतत",
-    "Whats the difference between a black father and boomerang? The boomerang comes back"
+    "What did the gods  say to the pharaoh? Hark, mere mortal, for you have betrayed your land. Because of this act, you shall be banished to a land of no return. ",
+    "What do you call a fake noodle? An impasta https://cdn.discordapp.com/attachments/1261166918639947778/1268626038003601431/image0.jpg?ex=66ad1b91&is=66abca11&hm=963dade5c383b762a090e83bb1606358fd3a8992c7d97d02a20a2628ff88b4ee&",
+    "Why did the tomato blush? Because he saw the salad dressing :joy_cat::sob::sob::rofl::rofl::rofl",
+    "What did the bee say when he couldn’t get in his hive? Un-bee-leavable",
+    "Why can’t you trust duck doctors? They’re all quacks",
+    "What did the ghost say when his favorite team lost? Boo!",
+    "What did one ocean say to the other ocean? Nothing, they just waved",
+    "Why can't a leopard hide? Because he's always spotted"
     ]
 if __name__ == "__main__":
     print("THIS IS THE MODULE DONT RUN THIS SCRIPT")
